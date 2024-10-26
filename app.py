45ce1089a4dbe68ee5e9b1b79a66238581bf9100
Nicholas_Miyasato <nicholetas@gmail.com>
2024-10-26 09:17:21 -0400

    prepare app for heroku simple deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template
 import pandas as pd
 
@@ -90,4 +91,5 @@ def filter_games():
         return "Invalid difficulty or category selected. Please try again."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
